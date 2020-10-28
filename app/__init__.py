640b9dca21cd588de88b89ef302e7308ca1df9cf
Kathleen-G <kathleen.guinee11@gmail.com>
2020-10-28 00:24:34 -0700

    added team page to website, added footer, and linked pages via the navbar

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def runit():
-    return render_template('home.html')
+    with app.open_resource('static/HomeResearchDescription.txt') as f:
+        content = f.read().decode('utf-8')
+    return render_template('home.html', description=content)
+
+@app.route('/researchteam/')
+def research_team():
+    return render_template('researchTeam.html')
 
 if __name__ == '__main__':
     app.run()
