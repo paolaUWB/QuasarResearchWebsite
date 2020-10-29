09763b4b4163e2d90f57460712578adc5aa9d08c
Kathleen-G <kathleen.guinee11@gmail.com>
2020-10-28 22:57:36 -0700

    created quasar research about page and fixed some UI bugs on the team member page

--- app/__init__.py
+++ app/__init__.py
@@ -16,5 +16,14 @@ def runit():
 def research_team():
     return render_template('researchTeam.html')
 
+@app.route('/quasarresearchabout/')
+def quasar_research_about():
+    with app.open_resource('static/QuasarResearchAboutPageDescription.txt') as f:
+        content = f.read().decode('utf-8')
+    return render_template('quasarResearchAbout.html', description=content)
+
+@app.route('/dataaccess/')
+def data_access():
+    return render_template('dataAccess.html')
 if __name__ == '__main__':
     app.run()
