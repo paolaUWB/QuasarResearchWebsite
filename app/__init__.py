244761d3c58b8162d912981c8a516f9ff88a220d
Audrey <59182366+nguyenaudrey@users.noreply.github.com>
2021-01-28 14:01:23 -0800

    edited _init_.py

--- app/__init__.py
+++ app/__init__.py
@@ -31,9 +31,10 @@ def connect_db():
             cursorclass = pymysql.cursors.DictCursor) 
     #local mysql server
     else:
+        print("hello")
         return pymysql.connect(
             host = 'localhost', user = 'root', password = os.environ.get('MYSQL_DATABASE_PASSWORD'),
-            database = 'quasarWebsite_db', autocommit = True, charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor) 
+            database = 'test', autocommit = True, charset = 'utf8mb4',cursorclass = pymysql.cursors.DictCursor) 
 
 def get_db():
     if not hasattr(g, 'db'):
@@ -85,215 +86,212 @@ def quasar_research_about():
 #Data access page
 @app.route('/dataaccess/', methods = ["GET", "POST"])
 def data_access():
-    try: 
-        form = DataAccessForm(request.form)
-        data = None
-        sql = None
+    form = DataAccessForm(request.form)
+    data = None
+    sql = None
 
-        with app.open_resource('static/descriptionText/DataAccessDescription.txt') as f:
-            content = f.read().decode('utf-8')
+    with app.open_resource('static/descriptionText/DataAccessDescription.txt') as f:
+        content = f.read().decode('utf-8')
 
-        if request.method == "POST":
-            if(form.Download.data):
-                #Get the QSO for all checked rows
-                downloadItems = request.form.getlist('downloadItem')
+    if request.method == "POST":
+        if(form.Download.data):
+            #Get the QSO for all checked rows
+            downloadItems = request.form.getlist('downloadItem')
 
-                if(downloadItems):
-                    #turn the checked items into string list for SQL query
-                    downloadItemsSQLList = "("
-                    count = 1
-                    for i in downloadItems:
-                        if(count != len(downloadItems)):
-                            downloadItemsSQLList = downloadItemsSQLList + "'" + i + "', "
-                        else:
-                            downloadItemsSQLList = downloadItemsSQLList + "'" + i + "')"
-                        count+=1
-
-                    sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE TRIM(t1.QSO) in %s" % (downloadItemsSQLList)
-                    cursor = get_db().cursor(pymysql.cursors.DictCursor)
-                    cursor.execute(sql)
-                    rows = cursor.fetchall()
-                    try:
-                        fp =open(os.path.join(APP_TMP, 'quasars.csv'), 'w', newline='')
-                    except Exception as e:
-                        return str(e)
-
-                    myFile = csv.writer(fp)
-                    myFile.writerow(rows[0].keys())
-                    for row in rows:
-                        myFile.writerow(row.values())
-                    fp.close()
-                    try:
-                        return send_file(os.path.join(APP_TMP, 'quasars.csv'), attachment_filename='quasars.csv', as_attachment=True)
-                    except Exception as e:
-                        return str(e)
-
-            elif(form.Submit.data):
-                cursor = get_db().cursor()
-
-                QSO = form.QSO.data
-                MDJ_FIBER = form.Plate_MJD_fiber.data
-                ZEMDR9Q_Min = form.ZEMDR9Q_Min.data
-                ZEMDR9Q_Max = form.ZEMDR9Q_Max.data
-                ZEMHW10_Min = form.ZEMHW10_Min.data
-                ZEMHW10_Max = form.ZEMHW10_Max.data
-                BALQSO = form.BALQSO.data
-
-                BI_EHVO_min = form.BI_EHVO_min.data
-                BI_EHVO_max = form.BI_EHVO_max.data
-                V_MAX = form.V_max.data
-                V_MIN = form.V_min.data
-                EW_min = form.EW_min.data
-                EW_max = form.EW_max.data
-
-                sql= None
-                vars = list()
-
-                #Check QSO
-                # select * from quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.QSO) ='J231227.48+005231.7';
-                if(len(QSO) > 0):
-                    sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.QSO) = %s"
-                    vars.append(QSO)
-
-                #check MJD_Fiber
-                if(MDJ_FIBER):
-                    if(sql):
-                        sql = sql + ' AND trim(PLATE_MJD_FIBER) = %s'
+            if(downloadItems):
+                #turn the checked items into string list for SQL query
+                downloadItemsSQLList = "("
+                count = 1
+                for i in downloadItems:
+                    if(count != len(downloadItems)):
+                        downloadItemsSQLList = downloadItemsSQLList + "'" + i + "', "
                     else:
-                        sql ='SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.PLATE_MJD_FIBER) = %s'
-                    vars.append(MDJ_FIBER)
+                        downloadItemsSQLList = downloadItemsSQLList + "'" + i + "')"
+                    count+=1
 
-                #Check ZEMDR9Q
-                if(ZEMDR9Q_Min and ZEMDR9Q_Max):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q BETWEEN %s AND %s'
-                    vars.append(ZEMDR9Q_Min)
-                    vars.append(ZEMDR9Q_Max)
+                sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE TRIM(t1.QSO) in %s" % (downloadItemsSQLList)
+                cursor = get_db().cursor(pymysql.cursors.DictCursor)
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+                try:
+                    fp =open(os.path.join(APP_TMP, 'quasars.csv'), 'w', newline='')
+                except Exception as e:
+                    return str(e)
 
-                elif (ZEMDR9Q_Min):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q >= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q >= %s'
-                    vars.append(ZEMDR9Q_Min)
+                myFile = csv.writer(fp)
+                myFile.writerow(rows[0].keys())
+                for row in rows:
+                    myFile.writerow(row.values())
+                fp.close()
+                try:
+                    return send_file(os.path.join(APP_TMP, 'quasars.csv'), attachment_filename='quasars.csv', as_attachment=True)
+                except Exception as e:
+                    return str(e)
 
-                elif (ZEMDR9Q_Max):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q <= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q <= %s '
-                    vars.append(ZEMDR9Q_Max)
+        elif(form.Submit.data):
+            cursor = get_db().cursor()
 
-                #Check ZEMHW10
-                if(ZEMHW10_Min and ZEMHW10_Max):
-                    if(sql):
-                        sql = sql + ' AND ZEMHW10 BETWEEN %s AND %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMHW10 BETWEEN %s AND %s'
-                    vars.append(ZEMHW10_Min)
-                    vars.append(ZEMHW10_Max)
+            QSO = form.QSO.data
+            MDJ_FIBER = form.Plate_MJD_fiber.data
+            ZEMDR9Q_Min = form.ZEMDR9Q_Min.data
+            ZEMDR9Q_Max = form.ZEMDR9Q_Max.data
+            ZEMHW10_Min = form.ZEMHW10_Min.data
+            ZEMHW10_Max = form.ZEMHW10_Max.data
+            BALQSO = form.BALQSO.data
 
-                elif (ZEMHW10_Min):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q >= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q >= %s'
-                    vars.append(ZEMHW10_Min)
+            BI_EHVO_min = form.BI_EHVO_min.data
+            BI_EHVO_max = form.BI_EHVO_max.data
+            V_MAX = form.V_max.data
+            V_MIN = form.V_min.data
+            EW_min = form.EW_min.data
+            EW_max = form.EW_max.data
 
-                elif (ZEMHW10_Max):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q <= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q <= %s '
-                    vars.append(ZEMHW10_Max)
+            sql= None
+            vars = list()
 
-                #Check BALQSO
-                if (BALQSO):
-                    if(sql):
-                        sql = sql + " AND trim(BALQSO) = %s"
-                    else:
-                        sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.BALQSO) = %s"
-                    vars.append(BALQSO)
+            #Check QSO
+            # select * from quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.QSO) ='J231227.48+005231.7';
+            if(len(QSO) > 0):
+                sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.QSO) = %s"
+                vars.append(QSO)
 
-                #Check BI_EHVO
-                if(BI_EHVO_min and BI_EHVO_max):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO BETWEEN %s AND %s'
-                    vars.append(BI_EHVO_min)
-                    vars.append(BI_EHVO_max)
+            #check MJD_Fiber
+            if(MDJ_FIBER):
+                if(sql):
+                    sql = sql + ' AND trim(PLATE_MJD_FIBER) = %s'
+                else:
+                    sql ='SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.PLATE_MJD_FIBER) = %s'
+                vars.append(MDJ_FIBER)
 
-                elif (BI_EHVO_min):
-                    if(sql):
-                        sql = sql + ' AND BI_EHVO >= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO >= %s'
-                    vars.append(BI_EHVO_min)
+            #Check ZEMDR9Q
+            if(ZEMDR9Q_Min and ZEMDR9Q_Max):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q BETWEEN %s AND %s'
+                vars.append(ZEMDR9Q_Min)
+                vars.append(ZEMDR9Q_Max)
 
-                elif (BI_EHVO_max):
-                    if(sql):
-                        sql = sql + ' AND BI_EHVO <= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO <= %s '
-                    vars.append(BI_EHVO_max)
+            elif (ZEMDR9Q_Min):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q >= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q >= %s'
+                vars.append(ZEMDR9Q_Min)
+
+            elif (ZEMDR9Q_Max):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q <= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q <= %s '
+                vars.append(ZEMDR9Q_Max)
+
+            #Check ZEMHW10
+            if(ZEMHW10_Min and ZEMHW10_Max):
+                if(sql):
+                    sql = sql + ' AND ZEMHW10 BETWEEN %s AND %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMHW10 BETWEEN %s AND %s'
+                vars.append(ZEMHW10_Min)
+                vars.append(ZEMHW10_Max)
+
+            elif (ZEMHW10_Min):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q >= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q >= %s'
+                vars.append(ZEMHW10_Min)
+
+            elif (ZEMHW10_Max):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q <= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t1.ZEMDR9Q <= %s '
+                vars.append(ZEMHW10_Max)
+
+            #Check BALQSO
+            if (BALQSO):
+                if(sql):
+                    sql = sql + " AND trim(BALQSO) = %s"
+                else:
+                    sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t1.BALQSO) = %s"
+                vars.append(BALQSO)
+
+            #Check BI_EHVO
+            if(BI_EHVO_min and BI_EHVO_max):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO BETWEEN %s AND %s'
+                vars.append(BI_EHVO_min)
+                vars.append(BI_EHVO_max)
+
+            elif (BI_EHVO_min):
+                if(sql):
+                    sql = sql + ' AND BI_EHVO >= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO >= %s'
+                vars.append(BI_EHVO_min)
+
+            elif (BI_EHVO_max):
+                if(sql):
+                    sql = sql + ' AND BI_EHVO <= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.BI_EHVO <= %s '
+                vars.append(BI_EHVO_max)
 
 
-                #Check V_Max
-                if(V_MAX):
-                    if(sql):
-                        sql = sql + " AND trim(t2.V_MAX) >= %s"
-                    else:
-                        sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t2.V_MAX) >= %s"
-                    vars.append(V_MAX)
+            #Check V_Max
+            if(V_MAX):
+                if(sql):
+                    sql = sql + " AND trim(t2.V_MAX) >= %s"
+                else:
+                    sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t2.V_MAX) >= %s"
+                vars.append(V_MAX)
+        
+            #Check V_min
+            if(V_MIN):
+                if(sql):
+                    sql = sql + " AND trim(t2.V_MIN) <= %s"
+                else:
+                    sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t2.V_MIN) <= %s"
+                vars.append(V_MIN)
+
+            #Check EW
+            if(EW_min and EW_max):
+                if(sql):
+                    sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW BETWEEN %s AND %s'
+                vars.append(EW_min)
+                vars.append(EW_max)
+
+            elif (EW_min):
+                if(sql):
+                    sql = sql + ' AND EW >= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW >= %s'
+                vars.append(EW_min)
+
+            elif (EW_max):
+                if(sql):
+                    sql = sql + ' AND EW <= %s'
+                else:
+                    sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW <= %s '
+                vars.append(BI_EHVO_max)
+    if(sql):
+        cursor = get_db().cursor()
+        cursor.execute(sql, tuple(vars))
+        data = cursor.fetchall()
+    else:
+        sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO"
+        cursor = get_db().cursor()
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+    if data:
+        return render_template('dataAccess.html', form=form, data=data, description=content)
             
-                #Check V_min
-                if(V_MIN):
-                    if(sql):
-                        sql = sql + " AND trim(t2.V_MIN) <= %s"
-                    else:
-                        sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE trim(t2.V_MIN) <= %s"
-                    vars.append(V_MIN)
-
-                #Check EW
-                if(EW_min and EW_max):
-                    if(sql):
-                        sql = sql + ' AND ZEMDR9Q BETWEEN %s AND %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW BETWEEN %s AND %s'
-                    vars.append(EW_min)
-                    vars.append(EW_max)
-
-                elif (EW_min):
-                    if(sql):
-                        sql = sql + ' AND EW >= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW >= %s'
-                    vars.append(EW_min)
-
-                elif (EW_max):
-                    if(sql):
-                        sql = sql + ' AND EW <= %s'
-                    else:
-                        sql = 'SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO WHERE t2.EW <= %s '
-                    vars.append(BI_EHVO_max)
-        if(sql):
-            cursor = get_db().cursor()
-            cursor.execute(sql, tuple(vars))
-            data = cursor.fetchall()
-        else:
-            sql = "SELECT * FROM quasarinfo t1 inner join quasarinfo_table2 t2 on t1.QSO = t2.QSO"
-            cursor = get_db().cursor()
-            cursor.execute(sql)
-            data = cursor.fetchall()
-
-        if data:
-            return render_template('dataAccess.html', form=form, data=data, description=content)
-
-    except Exception as e:
-        print(e)
     return render_template('dataAccess.html', form=form, description=content)
 
 #Test page
