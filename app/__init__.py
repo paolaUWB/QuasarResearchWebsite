9584a7a3fe1c107ad5ce4c28f34db32a5bb45780
Audrey <59182366+nguyenaudrey@users.noreply.github.com>
2021-02-12 14:46:10 -0800

    Restored port to OG number

--- app/__init__.py
+++ app/__init__.py
@@ -40,7 +40,7 @@ def connect_db():
         print("world")
         return pymysql.connect(
             host='vergil.u.washington.edu', user='root', password=os.environ.get('MYSQL_DATABASE_PASSWORD'),
-            database='quasarWebsite_db', autocommit=True, charset='utf8mb4', port= int(os.environ.get('MYSQL_DATABASE_PORT')),
+            database='quasarWebsite_db', autocommit=True, charset='utf8mb4', port=32445,
             cursorclass=pymysql.cursors.DictCursor)
     # local mysql server
     else:
